16aa956a3a5b32931229f14b0efd327386a4f9ee
Divyanshu Sahu <257divyanshu@gmail.com>
2026-01-08 17:52:54 +0530

    notes update for 08/01/26

---
+++ 01-basics/01-language-basics/05-intro-to-arrays-and-strings/notes.cpp
@@ -0,0 +1,20 @@
+// 📍 strings
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string str = "Hare Krishna";
+    cout<<str.length()<<endl;
+    cout<<str[0]<<endl;
+    return 0;
+};
+
+// 📍 creating an array
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int marks[5] = {91, 92, 93, 94, 95};
+//     cout << marks[0] << endl;
+//     return 0;
+// };
