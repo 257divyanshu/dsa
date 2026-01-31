89f863ba4851a7b31979929b856f084c7542c0f6
Divyanshu Sahu <257divyanshu@gmail.com>
2026-01-31 23:47:45 +0530

    notes update for 31/01/26

--- 08-bit-manipulation/13-single-number-ii/notes.cpp
+++ 08-bit-manipulation/13-single-number-ii/notes.cpp
@@ -10,48 +10,83 @@ long long customExponentiation(int a, int b)
     return result;
 };
 
+// 📍 bruteforce approach (sir's approach)
+class Solution
+{
+public:
+    int singleNumber(vector<int> &nums)
+    {
+        int result = 0;
+        for (int i = 0; i <= 31; i++)
+        {
+            // cout << " i : " << i << endl;
+            // cout << "current multiplier : " << multiplier << endl;
+            int bitSum = 0;
+            for (int j = 0; j < nums.size(); j++)
+            {
+                // cout << " j : " << j << endl;
+                // cout << "adding " << ((nums[j] >> i) & 1) << " to bitSum" <<
+                // endl;
+                if (nums[j] & (1 << i))
+                {
+                    bitSum++;
+                };
+            };
+            // cout << "bitSum : " << bitSum << endl;
+            if (bitSum % 3 == 1)
+            {
+                result = result | (1 << i);
+                // cout << "current multiplier : " << multiplier << endl;
+                // cout << "result updated : " << result << endl;
+            };
+        };
+        // cout << "final result : " << result << endl;
+        return result;
+    };
+};
+
 // 💡 hint 1
 // - If you sum up all the 1s at that specific bit position for every number in the array, what pattern or property would that sum have, considering every number appears exactly three times (except one)?
-// 📍 approach 1
-int singleNumber(vector<int> &nums)
-{
-    long long result = 0;
-    long long multiplier = 1;
-    for (int i = 0; i <= 31; i++)
-    {
-        bool stopFlag = true;
-        cout << " i : " << i << endl;
-        // cout << "current multiplier : " << multiplier << endl;
-        int bitSum = 0;
-        for (int j = 0; j < nums.size(); j++)
-        {
-            // cout << " j : " << j << endl;
-            // cout << "adding " << ((nums[j] >> i) & 1) << " to bitSum" << endl;
-            int rightShiftedValue = (nums[j] >> i);
-            cout << "rightShiftedValue " << rightShiftedValue << endl;
-            if (rightShiftedValue != 0)
-            {
-                stopFlag = false;
-            };
-            bitSum += (rightShiftedValue & 1);
-        };
-        if (stopFlag)
-        {
-            cout << "stopping" << endl;
-            break;
-        };
-        // cout << "bitSum : " << bitSum << endl;
-        if (bitSum % 3 == 1)
-        {
-            result += multiplier;
-            // cout << "current multiplier : " << multiplier << endl;
-            // cout << "result updated : " << result << endl;
-        };
-        multiplier *= 2;
-    };
-    cout << "final result : " << result << endl;
-    return result;
-};
+// 📍 my first approach
+// int singleNumber(vector<int> &nums)
+// {
+//     long long result = 0;
+//     long long multiplier = 1;
+//     for (int i = 0; i <= 31; i++)
+//     {
+//         bool stopFlag = true;
+//         cout << " i : " << i << endl;
+//         // cout << "current multiplier : " << multiplier << endl;
+//         int bitSum = 0;
+//         for (int j = 0; j < nums.size(); j++)
+//         {
+//             // cout << " j : " << j << endl;
+//             // cout << "adding " << ((nums[j] >> i) & 1) << " to bitSum" << endl;
+//             int rightShiftedValue = (nums[j] >> i);
+//             cout << "rightShiftedValue " << rightShiftedValue << endl;
+//             if (rightShiftedValue != 0)
+//             {
+//                 stopFlag = false;
+//             };
+//             bitSum += (rightShiftedValue & 1);
+//         };
+//         if (stopFlag)
+//         {
+//             cout << "stopping" << endl;
+//             break;
+//         };
+//         // cout << "bitSum : " << bitSum << endl;
+//         if (bitSum % 3 == 1)
+//         {
+//             result += multiplier;
+//             // cout << "current multiplier : " << multiplier << endl;
+//             // cout << "result updated : " << result << endl;
+//         };
+//         multiplier *= 2;
+//     };
+//     cout << "final result : " << result << endl;
+//     return result;
+// };
 
 int main()
 {
@@ -62,10 +97,9 @@ int main()
     // vector<int> nums = {2, 2, -1, 2};
 
     // cout << bitset<32>(INT32_MIN+1) << endl; // 10000000000000000000000000000001
-    // cout << bitset<32>((INT32_MIN+1) >> 1) << endl; // 
+    // cout << bitset<32>((INT32_MIN+1) >> 1) << endl; //
     // vector<int> nums = {2,2,2,INT32_MIN+1};
     // cout << singleNumber(nums) << endl;
-
 
     // 💡 why casting the result from long long to int worked magically!
 
@@ -77,7 +111,7 @@ int main()
     // cout << (int)(customExponentiation(2,32) - 1) << endl; // -1
 
     // 💡 have multiplier as a 64 bit long long is necessary
-    // - if we had multiplier as a 32 bit integer then, the multiplier updation statement would cause integer overflow error at i = 30 
+    // - if we had multiplier as a 32 bit integer then, the multiplier updation statement would cause integer overflow error at i = 30
 
     return 0;
 };
