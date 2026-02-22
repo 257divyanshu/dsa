eefcd68db219946854b016fac65d4a1da5230b64
Divyanshu Sahu <257divyanshu@gmail.com>
2026-02-22 14:03:05 +0530

    notes updae for 22/02/26

--- 00-extra/03-sorting-the-sentence/notes.cpp
+++ 00-extra/03-sorting-the-sentence/notes.cpp
@@ -1,33 +1,60 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+// 🏆 approach 1 (using vector)
+
+// 📍 approach 2 (using a hashmap)
+// - TC -> O(n * log(n)) (due to hashmap)
+// SC -> O(m) (where m is the length of string s)
+// string sortSentence(string s)
+// {
+//     istringstream ss(s);
+//     string word;
+//     map<int,string> map1;
+//     string sentence = "";
+//     while (ss >> word) // TC -> O(n) ( O(9) in worst case )
+//     {
+//         // cout << word << endl;
+//         // cout << word[word.length()-1] << endl;
+//         // cout << (int)(word[word.length()-1] - '1') << endl;
+//         map1[(int)(word[word.length() - 1] - '1')] = word.substr(0, word.length() - 1); // TC -> O(log(n)) ( O(log(9)) in worst case )
+//     }
+//     for(auto it : map1){ // TC -> O(n) ( O(9) in worst case )
+//         if(sentence!=""){
+//             sentence += " ";
+//         }
+//         sentence += it.second;
+//     }
+//     return sentence;
+// }
+
+// 📍 approach 1 (using a vector)
+// TC -> O(n)
+// SC -> O(m) (where m is the length of string s)
 string sortSentence(string s)
 {
-
     istringstream ss(s);
     string word;
     vector<string> wordVector(9);
     string sentence = "";
-    int counter = 1;
-
-    while (ss >> word)
+    // int counter = 1;
+    while (ss >> word) // TC -> O(n) ( O(9) in worst case )
     {
         // cout << "iteration : " << counter << endl;
-        counter++;
         // cout << word << endl;
-        // cout << word[word.length()-1] << endl;
-        // cout << (int)(word[word.length()-1] - '1') << endl;
-        wordVector[(int)(word[word.length() - 1] - '1')] = word.substr(0, word.length() - 1);
+        // counter++;
+        int index = word.back() - '1';
+        word.pop_back();
+        // cout << "index " << index << "; word " << word << endl;
+        wordVector[index] = word; // O(1)
     }
-
     // for(auto wordd : wordVector){
     //     cout << wordd << " ";
     // }
     // cout << endl;
-
     // cout << wordVector.size() << endl;
-
     sentence = wordVector[0];
-    for (size_t i = 1; i < wordVector.size(); i++)
+    for (size_t i = 1; i < wordVector.size(); i++) // TC -> O(n) ( O(9) in worst case )
     {
         if (wordVector[i] != "")
         {
@@ -35,18 +62,18 @@ string sortSentence(string s)
             sentence += " " + wordVector[i];
         }
     }
-
     return sentence;
 }
+
 int main()
 {
-    // string s = "is2 sentence4 This1 a3";
-    // cout << "|" << sortSentence(s) << "|" << endl;
-    string s = "";
+    string s = "is2 sentence4 This1 a3";
+    cout << "|" << sortSentence(s) << "|" << endl;
+    // string s = "";
     // cout << "|" << s[2] << "|" << endl;
     // cout << "|" <<  "|" << endl;
-    s[1] = 'x';
-    cout << s << endl;
-    cout << s[1] << endl;
+    // s[1] = 'x';
+    // cout << s << endl;
+    // cout << s[1] << endl;
     return 0;
 };
