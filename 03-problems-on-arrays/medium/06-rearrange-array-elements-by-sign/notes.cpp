c09f6216c9629907c31f850dd41d61aa1bff93dc
Divyanshu Sahu <257divyanshu@gmail.com>
2026-03-07 14:55:03 +0530

    notes update for 07/03/26

--- 03-problems-on-arrays/medium/06-rearrange-array-elements-by-sign/notes.cpp
+++ 03-problems-on-arrays/medium/06-rearrange-array-elements-by-sign/notes.cpp
@@ -1,32 +1,82 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// 📍 optimal approach
-// - TC -> O(n) (single pass)
+// 📍 VARIATION : if its not guaranteed that the number of positives is equal to the number of negatives
+// 📍 approach (from bruteforce's intution)
+// - TC -> O(n) (O(2*n)) more precisely) (see 📸 time-complexity-analysis.png)
 // - SC -> O(n)
 vector<int> rearrangeArray(vector<int> &nums)
 {
-    vector<int> resultantVector(nums.size(), 0);
-    int positiveIndex = 0;
-    int negativeIndex = 1;
+    vector<int> positives(0);
+    vector<int> negatives(0);
     for (int i = 0; i < nums.size(); i++)
     {
         if (nums[i] > 0)
         {
-            resultantVector[positiveIndex] = nums[i];
-            positiveIndex += 2;
+            positives.push_back(nums[i]);
         }
         else
         {
-            resultantVector[negativeIndex] = nums[i];
-            negativeIndex += 2;
+            negatives.push_back(nums[i]);
         }
     }
-    return resultantVector;
+    int sign = 0;
+    int index = nums.size() / 2;
+    if (positives.size() > negatives.size())
+    {
+        sign = 1;
+        index = negatives.size();
+    }
+    else if (positives.size() < negatives.size())
+    {
+        sign = -1;
+        index = positives.size();
+    }
+    for (int i = 0; i < index; i++)
+    {
+        nums[2 * i] = positives[i];
+        nums[2 * i + 1] = negatives[i];
+    }
+    for (int i = index * 2; i < nums.size(); i++)
+    {
+        if (sign == 1)
+        {
+            nums[i] = positives[i - index];
+        }
+        else
+        {
+            nums[i] = negatives[i - index];
+        }
+    }
+    return nums;
 }
 
+// 📍 optimal approach
+// - TC -> O(n) (single pass)
+// - SC -> O(n)
+// vector<int> rearrangeArray(vector<int> &nums)
+// {
+//     vector<int> resultantVector(nums.size(), 0);
+//     int positiveIndex = 0;
+//     int negativeIndex = 1;
+//     for (int i = 0; i < nums.size(); i++)
+//     {
+//         if (nums[i] > 0)
+//         {
+//             resultantVector[positiveIndex] = nums[i];
+//             positiveIndex += 2;
+//         }
+//         else
+//         {
+//             resultantVector[negativeIndex] = nums[i];
+//             negativeIndex += 2;
+//         }
+//     }
+//     return resultantVector;
+// }
+
 // 📍 bruteforce approach
-// - TC -> O(n) (O(2*n) more precisely)
+// - TC -> O(n) (O((3*n)/2) more precisely)
 // - SC -> O(n)
 // vector<int> rearrangeArray(vector<int> &nums)
 // {
