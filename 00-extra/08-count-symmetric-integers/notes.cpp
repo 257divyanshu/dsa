d5ef5d6223005b9160a09d601daf0f80d8fb39a5
Divyanshu Sahu <257divyanshu@gmail.com>
2026-08-09 23:54:22 +0530

    extra questions' notes update

--- 00-extra/08-count-symmetric-integers/notes.cpp
+++ 00-extra/08-count-symmetric-integers/notes.cpp
@@ -1,1 +1,9 @@
-// - optimization pending
+// 📍 Benefits of static_cast
+// - Clearly indicates that you're performing a C++ type conversion.
+// - Easier to search for in code (static_cast).
+// - Doesn't allow some unsafe conversions that C-style casts can perform.
+// - Recommended by the C++ Core Guidelines.
+
+// 📝 lessons:
+// - while calculating complexities, use pen and paper (for better accuracy)
+// - while calculating complexities, have a look at the constraints
---
+++ 00-extra/09-reverse-bits/notes.cpp
@@ -0,0 +1,3 @@
+// - the same 32-bit pattern can represent different numbers depending on whether we interpret it as signed or unsigned.
+
+// The idea is: take bits from n from right → left, and build result from left → right.
