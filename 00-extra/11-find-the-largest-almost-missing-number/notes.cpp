068b93e587df97aa7f0f7d0710ad5ebbd537a1f2
Divyanshu Sahu <257divyanshu@gmail.com>
2026-08-11 22:07:18 +0530

    extra question notes update for LC 3471

--- 00-extra/11-find-the-largest-almost-missing-number/notes.cpp
+++ 00-extra/11-find-the-largest-almost-missing-number/notes.cpp
@@ -1,3 +1,6 @@
 // it was a really really nice question
 
-// complexity analysis and optimization left
+// 📍 the logic summarized in few lines:
+// k == 1 → Find the largest number with frequency 1.
+// 1 < k < n → Only nums[0] and nums[n-1] can be almost missing; return the larger one that appears exactly once.
+// k == n → Every number appears in the only subarray, so return the maximum element.
