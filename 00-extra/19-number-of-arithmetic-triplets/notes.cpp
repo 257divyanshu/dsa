f5618b15ffdfaabf7894af10ba14ac5d5c293322
Divyanshu Sahu <257divyanshu@gmail.com>
2026-08-22 19:01:16 +0530

    notes update for LC 2367

---
+++ 00-extra/19-number-of-arithmetic-triplets/notes.cpp
@@ -0,0 +1,8 @@
+// 📍 learning
+// - complexities need to be calculated on paper with pen
+// - trying to calculate them in mind introduces chances of error in problems involving complex logic
+
+// 📍 logs
+// - first came up with a solution that uses additional data structure (a hashmap) (using unordered_map), it had TC O(n)
+// - then came up with a solution that doesn't use any extra space, but its TC grew to O(n^2) (and this is where my complexity analysis went wrong) (because I didn't calculate it on paper)
+// - first solution is the optimal one
