995ed90aa794fc7e1b59c581b449af3e002ab205
Divyanshu Sahu <257divyanshu@gmail.com>
2026-08-24 21:58:23 +0530

    notes update for LC 2006

---
+++ 00-extra/20-count-the-number-of-pairs-with-absolute-difference-k/notes.cpp
@@ -0,0 +1,10 @@
+// QUESTION NO 2006
+
+// came up with two approaches:
+// - first one was TC O(n^2) and SC O(1) brute-force
+// - second one was TC O(n) and SC O(n) optimal
+
+// 📍 .count() on unordered_map
+// - checks whether a key exists in the map.
+// - Returns 1 if key exists
+// - Returns 0 if key doesn't exist
