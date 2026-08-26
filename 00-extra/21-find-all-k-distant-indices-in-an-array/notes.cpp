4a2a45167a2130d275c5643160b81646a6e39f30
Divyanshu Sahu <257divyanshu@gmail.com>
2026-08-26 17:14:37 +0530

    notes update for LC 2200

--- 00-extra/21-find-all-k-distant-indices-in-an-array/notes.cpp
+++ 00-extra/21-find-all-k-distant-indices-in-an-array/notes.cpp
@@ -5,3 +5,16 @@
 //   - Outer loop: O(n)
 //   - If many elements equal key, the inner loop can iterate up to O(n) for each one.
 // - SC -> O(n) (including the output array)
+
+// 📍 time complexity of 
+// - push_back() -> O(1) (ammortized)
+// - pop_back() -> O(1)
+// - pop_front() -> O(n)
+
+// - couldn't come up with a better approach, even after hints
+
+// 📍 studied the better approach
+// - it involves "preprocessing + binary search"
+// - couldn't think that the solution might involve "binary search"
+// - TC -> O(n * log(n)) 
+// - SC -> O(n)
